9e1779bbc3791e8f14b2bf37d2ad1fb5f562be45
Bruno Marques <bruno.oliveira_marques@siemens.com>
2024-08-08 17:17:07 +0100

    Added fonts

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,6 +2,8 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
+#include "fonts.h"
+
 #define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
 #define MAX_DEVICES   10
 #define CS_PIN        10
@@ -10,6 +12,7 @@ auto P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 void setup() {
   P.begin();
+  P.setFont(fontIbm);
 }
 
 void loop() {
