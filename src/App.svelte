8998c9983020e28b5b3d00ea2728748e40562995
Bruno Marques <bruno.oliveira_marques@siemens.com>
2024-08-12 18:04:48 +0100

    Created LED matrix component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
   import AVRRunner from './execute';
+  import LedMatrix from './lib/LedMatrix.svelte';
   import firmwareUrl from '../build/firmware.ino.with_bootloader.bin?url';
 
   const CHAIN_LENGTH = 10;
-  const OFF_COLOR = '#444444';
-  const color = '#ff9900';
-  const ledWidth = 4;
-  const ledHeight = 5;
 
   let matrix: Uint8Array[] = [];
 
@@ -44,15 +41,5 @@
 </script>
 
 <main>
-  {#each matrix as display}
-    <svg xmlns="http://www.w3.org/2000/svg" width={ledWidth * 8} height={ledHeight * 8}>
-      {#each display as row, y}
-        {#each Array(8) as _, x}
-          <ellipse fill={(row >> x) & 1 ? color : OFF_COLOR}
-                   rx={ledWidth / 2} ry={ledHeight / 2}
-                   cx={ledWidth * (x + 0.5)} cy={ledHeight * (y + 0.5)}/>
-        {/each}
-      {/each}
-    </svg>
-  {/each}
+  <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} matrix={matrix} />
 </main>
---
+++ src/lib/LedMatrix.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  const OFF_COLOR = '#444444';
+
+  export let ledWidth: number;
+  export let ledHeight: number;
+  export let color: string;
+  export let matrix: Uint8Array[];
+</script>
+
+{#each matrix as display}
+  <svg xmlns="http://www.w3.org/2000/svg" width={ledWidth * 8} height={ledHeight * 8}>
+    {#each display as row, y}
+      {#each Array(8) as _, x}
+        <ellipse fill={(row >> x) & 1 ? color : OFF_COLOR}
+                rx={ledWidth / 2} ry={ledHeight / 2}
+                cx={ledWidth * (x + 0.5)} cy={ledHeight * (y + 0.5)}/>
+      {/each}
+    {/each}
+  </svg>
+{/each}
