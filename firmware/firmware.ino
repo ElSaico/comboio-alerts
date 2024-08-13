2761a1af80caca78950d1485db96fa1764e078b9
Bruno Marques <bruno.oliveira_marques@siemens.com>
2024-08-13 17:18:36 +0100

    Started zoning

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,17 +5,25 @@
 #include "fonts.hpp"
 
 #define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
-#define MAX_DEVICES   10
+#define MAX_DEVICES   54
 #define CS_PIN        10
 
 auto P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+String labels[3];
 
 void setup() {
-  P.begin();
-  P.setFont(fontIbm);
+  Serial.begin(9600);
+  P.begin(4);
+  P.setZone(0,  0, 23);
+  P.setZone(1, 24, 33);
+  P.setZone(2, 34, 43);
+  P.setZone(3, 44, 53);
+  P.displayReset();
+  P.displayZoneText(1, "Hello World!", PA_CENTER, P.getSpeed(), 1000, PA_SCROLL_DOWN, PA_SCROLL_DOWN);
 }
 
 void loop() {
-  if (P.displayAnimate())
-    P.displayText("Hello World!", PA_CENTER, P.getSpeed(), 1000, PA_SCROLL_DOWN, PA_SCROLL_DOWN);
+  if (P.displayAnimate()) {
+    P.getZoneStatus(0);
+  }
 }
--- src/App.svelte
+++ src/App.svelte
@@ -3,31 +3,36 @@
   import LedMatrix from './lib/LedMatrix.svelte';
   import firmwareUrl from '../build/firmware.ino.with_bootloader.bin?url';
 
-  const CHAIN_LENGTH = 10;
+  const ZONES = [
+    { start:  0, end: 23 },
+    { start: 24, end: 33 },
+    { start: 34, end: 43 },
+    { start: 44, end: 53 },
+  ];
+  const TOTAL_MODULES = 54;
 
-  let matrix: Uint8Array[] = [];
+  const matrix: Uint8Array[] = new Array(TOTAL_MODULES);
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = new Uint8Array(8);
+  }
 
   fetch(firmwareUrl).then(async response => {
     const firmware = await response.arrayBuffer();
     const runner = new AVRRunner(new Uint16Array(firmware));
     let display = 0;
-    let segment: number | null = null;
-
-    for (let i = 0; i < CHAIN_LENGTH; i++) {
-      matrix.push(new Uint8Array(8));
-    }
+    let opcode: number | null = null;
 
     runner.spi.onByte = value => {
-      if (segment === null) {
-        segment = value;
+      if (opcode === null) {
+        opcode = value;
       } else {
-        if (segment <= 8) {
-          matrix[display][segment-1] = value;
+        if (opcode <= 8) {
+          matrix[display][opcode-1] = value;
         } else {
-          console.log('out of bounds: %s %s', segment, value);
+          console.log('unhandled opcode: %s %s', opcode, value);
         }
-        display = (display + 1) % CHAIN_LENGTH;
-        segment = null;
+        display = (display + 1) % TOTAL_MODULES;
+        opcode = null;
       }
       runner.spi.completeTransfer(value);
     };
