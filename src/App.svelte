5a8758810ec71bdd648df90209bfdce0b2abcec2
Bruno Marques <bruno@bmarques.net>
2024-08-17 23:18:08 +0100

    Serial support on frontend

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,20 @@
   import LedMatrix from './lib/LedMatrix.svelte';
   import firmwareUrl from '../build/firmware.ino.with_bootloader.bin?url';
 
+  let serialInput = '';
+  let runner: AVRRunner;
+
   fetch(firmwareUrl).then(async response => {
     const firmware = await response.arrayBuffer();
-    const runner = new AVRRunner(new Uint16Array(firmware));
+    runner = new AVRRunner(new Uint16Array(firmware));
 
     runner.spi.onByte = value => {
       matrix.set(value);
       runner.spi.completeTransfer(value);
+    };
+
+    runner.usart.onLineTransmit = line => {
+      console.log(line);
     };
 
     runner.execute(() => {});
@@ -26,4 +33,9 @@
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(1)} />
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(2)} />
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(3)} />
+  <input type="text" bind:value={serialInput} />
+  <button type="button" on:click={() => {
+    runner.serialWrite(serialInput + '\n');
+    serialInput = '';
+  }}>Send</button>
 </main>
--- src/execute.ts
+++ src/execute.ts
@@ -1,96 +1,42 @@
-/**
- * AVRRunner
- * Part of AVR8js
- *
- * Copyright (C) 2019, Uri Shaked
- */
 import {
   avrInstruction,
-  avrInterrupt,
-  AVRTimer,
   CPU,
-  AVRIOPort,
   AVRUSART,
   AVRSPI,
-  AVRTWI,
-  portBConfig,
-  portCConfig,
-  portDConfig,
-  timer0Config,
-  timer1Config,
-  timer2Config,
   usart0Config,
   spiConfig,
-  twiConfig,
 } from 'avr8js';
 
-import { MicroTaskScheduler } from './task-scheduler';
-
 export default class AVRRunner {
-  readonly program: Uint16Array;
   readonly cpu: CPU;
-  readonly timer0: AVRTimer;
-  readonly timer1: AVRTimer;
-  readonly timer2: AVRTimer;
-  readonly portB: AVRIOPort;
-  readonly portC: AVRIOPort;
-  readonly portD: AVRIOPort;
   readonly usart: AVRUSART;
   readonly spi: AVRSPI;
-  readonly twi: AVRTWI;
   readonly frequency = 16e6; // 16 MHz
-  readonly taskScheduler = new MicroTaskScheduler();
 
   private serialBuffer: Array<number> = [];
   private stopped = false;
 
   constructor(program: Uint16Array) {
-    this.program = program;
-    this.cpu = new CPU(this.program);
-
-    this.timer0 = new AVRTimer(this.cpu, timer0Config);
-    this.timer1 = new AVRTimer(this.cpu, timer1Config);
-    this.timer2 = new AVRTimer(this.cpu, timer2Config);
-
-    this.portB = new AVRIOPort(this.cpu, portBConfig);
-    this.portC = new AVRIOPort(this.cpu, portCConfig);
-    this.portD = new AVRIOPort(this.cpu, portDConfig);
-
+    this.cpu = new CPU(program);
     this.usart = new AVRUSART(this.cpu, usart0Config, this.frequency);
     this.spi = new AVRSPI(this.cpu, spiConfig, this.frequency);
-    this.twi = new AVRTWI(this.cpu, twiConfig, this.frequency);
 
-    // this.serialOnLineTransmit();
-    this.cpu.readHooks[usart0Config.UDR] = () => this.serialBuffer.shift() || 0;
-
-    this.taskScheduler.start();
-  }
-
-  // Function to send data to the serial port
-  serialWrite(value: string) {
-    // Writing to UDR transmits the byte
-    [...value].forEach(c => {
-      // Write a character
-      this.serialBuffer.push(c.charCodeAt(0));
-    });
-  }
-
-  serialOnLineTransmit() {
-    // Serial port to browser console
-    this.usart.onLineTransmit = (line) => {
-      console.log("[Serial] %c%s", "color: red", line);
+    this.usart.onRxComplete = () => {
+      if (this.serialBuffer.length > 0) {
+        this.usart.writeByte(this.serialBuffer.shift()!);
+      }
     };
   }
 
-  rxCompleteInterrupt() {
-    const UCSRA = this.cpu.data[usart0Config.UCSRA];
-
-    if ((UCSRA & 0x20) && (this.serialBuffer.length > 0)) {
-      avrInterrupt(this.cpu, usart0Config.rxCompleteInterrupt);
+  serialWrite(value: string) {
+    for (let c of value) {
+      this.serialBuffer.push(c.charCodeAt(0));
+    };
+    if (!this.usart.rxBusy) {
+      this.usart.onRxComplete!();
     }
   }
 
-  // CPU main loop
   execute(callback: (cpu: CPU) => void) {
     if (this.stopped) {
       return;
@@ -110,21 +56,5 @@ export default class AVRRunner {
 
   stop() {
     this.stopped = true;
-  }
-
-  analogPort() {
-    // Simulate analog port (so that analogRead() eventually return)
-    this.cpu.writeHooks[0x7a] = value => {
-      if (value & (1 << 6)) {
-        // random value
-        const analogValue = Math.floor(Math.random() * 1024);
-
-        this.cpu.data[0x7a] = value & ~(1 << 6); // Clear bit - conversion done
-        this.cpu.data[0x78] = analogValue & 0xff;
-        this.cpu.data[0x79] = (analogValue >> 8) & 0x3;
-
-        return true; // Don't update
-      }
-    };
   }
 }
