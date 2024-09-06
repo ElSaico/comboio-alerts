48776aa4773a1e401140a5c853952bee0fb6d39e
Bruno Marques <bruno.oliveira_marques@siemens.com>
2024-09-06 16:04:29 +0100

    Replaced serial input with fixed messages

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,18 @@
   import LedMatrix from './lib/LedMatrix.svelte';
   import firmwareUrl from '../build/firmware.ino.with_bootloader.bin?url';
 
-  let serialInput = '';
+  const messages = [
+    ['Alert: Follow', 'AF0\nTestFollow\n'],
+    ['Alert: Sub', 'AS0\nTestSub\nNew sub message\n'],
+    ['Alert: Resub', 'As12\nTestResub\nResub message\n'],
+    ['Alert: Gift', 'AG10\nTestGiftSub\n'],
+    ['Alert: Cheer', 'AC1000\nTestCheer\nCheer message\n'],
+    ['Alert: Raid', 'AR10000\nTestRaid\nRaid Test\n'],
+    ['Label: Follow', 'LF0\nTestFollow\n'],
+    ['Label: Sub', 'LS0\nTestSub\n'],
+    ['Label: Resub', 'Ls24\nTestResub\n'],
+    ['Label: Cheer', 'LC1000\nTestCheer\n'],
+  ];
   let runner: AVRRunner;
 
   fetch(firmwareUrl).then(async response => {
@@ -33,9 +44,7 @@
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(1)} />
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(2)} />
   <LedMatrix color="#ff9900" ledWidth={4} ledHeight={5} modules={matrix.zone(3)} />
-  <input type="text" bind:value={serialInput} />
-  <button type="button" on:click={() => {
-    runner.serialWrite(serialInput + '\n');
-    serialInput = '';
-  }}>Send</button>
+  {#each messages as message}
+    <button type="button" on:click={() => { runner.serialWrite(message[1]); }}>{message[0]}</button>
+  {/each}
 </main>
