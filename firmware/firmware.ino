6ccb374c8a1969944e64400bbb9209367831730e
Bruno Marques <bruno@bmarques.net>
2024-10-29 19:38:44 +0000

    Dropped classes (still a positive outcome as the code got cleaner)

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,36 +2,108 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
-#include "display.hpp"
 #include "fonts.hpp"
 
-#define BAUD_RATE     9600
-#define CS_PIN        10
+#define BAUD_RATE      9600
+#define CS_PIN         10
 
-#define NUM_ZONES     4
-#define NUM_MODULES   54
+#define NUM_ZONES      4
+#define NUM_MODULES    54
+
+#define MAX_LABEL      36 // up to 25 for username + 4 for " ()\0"; this leaves a minimum of 7 for amount
+#define MAX_ALERT      64
+#define MAX_MESSAGE    512
+
+#define SIZE_ALERTS    24
+#define SIZE_LABEL     10
+
+#define LABEL_SPEED    50
+
+#define LABEL_START(z) (SIZE_ALERTS + SIZE_LABEL*((z) - 1))
+#define LABEL_END(z)   (SIZE_ALERTS + SIZE_LABEL*(z) - 1)
 
 #define TARGET_ALERTS 'A'
 #define TARGET_LABEL  'L'
 
 enum InputStage { INPUT_TARGET, INPUT_EVENT, INPUT_NUMBER, INPUT_USER, INPUT_MESSAGE };
+enum MatrixZone { ZONE_ALERTS, ZONE_FOLLOW, ZONE_SUB, ZONE_CHEER };
+enum AlertStage { ALERT_IDLE, ALERT_EVENT, ALERT_USER, ALERT_MESSAGE };
+enum EventType : char {
+  EVENT_FOLLOW = 'F',
+  EVENT_SUB_NEW = 'S',
+  EVENT_SUB_RENEW = 's',
+  EVENT_SUB_GIFT = 'G',
+  EVENT_CHEER = 'C',
+  EVENT_RAID = 'R',
+  EVENT_DONATE = 'D',
+  EVENT_SHOUTOUT = 'O',
+};
+
+const char ALERTS_FOLLOW[] PROGMEM = "Novo passageiro no Comboio";
+// TODO pluralize
+const char ALERTS_RAID[] PROGMEM = "Embarque de uma raid com %d pessoas";
+const char ALERTS_SUB_GIFT[] PROGMEM = "%d passes doados para o Comboio";
+const char ALERTS_SUB_NEW[] PROGMEM = "Nova aquisição de passe";
+const char ALERTS_SUB_RENEW[] PROGMEM = "Renovação de passe, totalizando %d meses";
+// TODO check how to handle decimal values
+const char ALERTS_DONATE[] PROGMEM = "Pix de %s enviado para o Comboio";
+const char ALERTS_SHOUTOUT[] PROGMEM = "O Comboio do Saico recomenda este canal";
 
 auto P = MD_Parola(MD_MAX72XX::PAROLA_HW, CS_PIN, NUM_MODULES);
 char userBuffer[MAX_LABEL];
 char messageBuffer[MAX_MESSAGE];
+char labelBuffer[NUM_ZONES-1][MAX_LABEL];
+char alertBuffer[MAX_ALERT];
+bool alertMessage;
+auto alertStage = ALERT_IDLE;
 
-auto alerts = AlertDisplay(&P, userBuffer, messageBuffer);
-auto labelFollow = LabelDisplay(&P, ZONE_FOLLOW);
-auto labelSub = LabelDisplay(&P, ZONE_SUB);
-auto labelCheer = LabelDisplay(&P, ZONE_CHEER);
+void beginAlert() {
+  P.setZone(ZONE_ALERTS, 0, SIZE_ALERTS-1);
+  P.setSpeed(ZONE_ALERTS, LABEL_SPEED);
+  P.setTextAlignment(ZONE_ALERTS, PA_CENTER);
+  P.setPause(ZONE_ALERTS, 0);
+}
+
+void beginLabel(MatrixZone z) {
+  P.setZone(z, LABEL_START(z), LABEL_END(z));
+  P.setSpeed(z, LABEL_SPEED);
+  P.setTextAlignment(z, PA_CENTER);
+  P.setFont(z, FONT_METRO);
+  P.setTextBuffer(z, labelBuffer[z-1]);
+  P.setPause(z, LABEL_SPEED);
+}
 
 void setup() {
   Serial.begin(BAUD_RATE);
   P.begin(NUM_ZONES);
-  alerts.begin();
-  labelFollow.begin();
-  labelSub.begin();
-  labelCheer.begin();
+  beginAlert();
+  beginLabel(ZONE_FOLLOW);
+  beginLabel(ZONE_SUB);
+  beginLabel(ZONE_CHEER);
+}
+
+void setAlert(EventType eventType, uint32_t num) {
+  alertStage = ALERT_EVENT;
+  // TODO set alertBuffer and alertMessage according to eventType
+  P.setFont(ZONE_ALERTS, FONT_METRO);
+  P.setTextBuffer(ZONE_ALERTS, alertBuffer);
+  P.setTextEffect(ZONE_ALERTS, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  P.displayReset(ZONE_ALERTS);
+}
+
+void setLabel(MatrixZone z, uint32_t num) {
+  if (num == 0) {
+    strncpy(labelBuffer[z-1], userBuffer, MAX_LABEL);
+  } else {
+    snprintf_P(labelBuffer[z-1], MAX_LABEL, PSTR("%s (%d)"), userBuffer, num);
+  }
+
+  if (P.getTextColumns(z, labelBuffer[z-1]) > SIZE_LABEL*8) {
+    P.setTextEffect(z, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  } else {
+    P.setTextEffect(z, PA_PRINT, PA_NO_EFFECT);
+  }
+  P.displayReset(z);
 }
 
 void readSerial() {
@@ -73,7 +145,7 @@ void readSerial() {
           switch (eventType) {
             case EVENT_FOLLOW: case EVENT_SUB_GIFT:
               stage = INPUT_TARGET;
-              alerts.set(eventType, num);
+              setAlert(eventType, num);
               break;
             case EVENT_SUB_NEW: case EVENT_SUB_RENEW: case EVENT_CHEER: case EVENT_RAID:
               stage = INPUT_MESSAGE;
@@ -84,21 +156,17 @@ void readSerial() {
           }
         } else {
           stage = INPUT_TARGET;
-          LabelDisplay* label;
           switch (eventType) {
             case EVENT_FOLLOW:
-              label = &labelFollow;
+              setLabel(ZONE_FOLLOW, num);
               break;
             case EVENT_SUB_NEW: case EVENT_SUB_RENEW: case EVENT_SUB_GIFT:
-              label = &labelSub;
+              setLabel(ZONE_SUB, num);
               break;
             case EVENT_CHEER:
-              label = &labelCheer;
+              setLabel(ZONE_CHEER, num);
               break;
-            default:
-              return;
           }
-          label->set(userBuffer, num);
         }
       }
       break;
@@ -108,9 +176,23 @@ void readSerial() {
       } else {
         messageBuffer[messageSize] = '\0';
         stage = INPUT_TARGET;
-        alerts.set(eventType, num);
+        setAlert(eventType, num);
       }
       break;
+  }
+}
+
+void animateAlert() {
+  if (P.getZoneStatus(ZONE_ALERTS)) {
+    // TODO state management
+    // TODO handle timed rewards
+    // TODO send callbacks to the bot (how?), for TTS and queue unlock
+  }
+}
+
+void animateLabel(MatrixZone z) {
+  if (P.getZoneStatus(z)) {
+    P.displayReset(z);
   }
 }
 
@@ -120,9 +202,9 @@ void loop() {
   }
 
   if (P.displayAnimate()) {
-    alerts.animate();
-    labelFollow.animate();
-    labelSub.animate();
-    labelCheer.animate();
+    animateAlert();
+    animateLabel(ZONE_FOLLOW);
+    animateLabel(ZONE_SUB);
+    animateLabel(ZONE_CHEER);
   }
 }
